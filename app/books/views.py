546bb73ca0b9a024e0f65d7d5b50e6c8b3e0cd45
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-01-30 16:59:16 +0100

    Basic book list view without pagination and filtering

--- app/books/views.py
+++ app/books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from . import models
+
+
+class BookListView(generic.ListView):
+    queryset = models.Book.objects.select_related().all()
+    context_object_name = "books"
