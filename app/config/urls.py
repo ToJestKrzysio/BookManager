8d075829654d51778e612eb1ea0719e7244d4ea0
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-03-28 13:40:40 +0200

    Initial configuration of DRF and swagger

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BookManager API",
+        default_version='v1',
+        description="Following documentation provides information about available REST endpoint "
+                    "for BookManager application.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="krzysztof.plonka64@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
